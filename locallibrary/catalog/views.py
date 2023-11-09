3a73ceb1957bb519190c2fa0e0949452b02d2bf0
79234 <marusya0323@gmail.com>
2023-11-10 02:46:38 +0700

    Отображение списков и детальной информации

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from .models import Book, Author, BookInstance, Genre
@@ -22,3 +22,26 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
