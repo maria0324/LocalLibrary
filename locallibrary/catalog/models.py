d7ac101ca15f53a3038e4ab551b43063a92ebd60
79234 <marusya0323@gmail.com>
2023-11-10 01:52:44 +0700

    Административная панель Django

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -45,6 +45,14 @@ class Book(models.Model):
         """
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
 
 import uuid # Required for unique book instances
 
@@ -98,5 +106,8 @@ class Author(models.Model):
         String for representing the Model object.
         """
         return '%s, %s' % (self.last_name, self.first_name)
+
+
+
 
 
